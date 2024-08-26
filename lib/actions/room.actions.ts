51848f99a1cd79ba54a9d2b52794ac298451cf8d
shreenidhi deshpande <shreenidhideshpande4u@gmail.com>
2024-08-26 17:52:02 +0530

    project is enterprise ready with sentry

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,85 @@
+'use server';
+
+import { nanoid } from 'nanoid';
+import liveblocks from '../liveblocks';
+import { revalidatePath } from 'next/cache';
+import { parseStringify } from '../utils';
+
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+  try {
+    const metadata = {
+      createId: userId,
+      email,
+      title: 'Untitled',
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ['room:write'],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ['room:write'],
+    });
+
+    revalidatePath('/');
+
+    return parseStringify(room);
+  } catch (e) {
+    console.log(`Error occured while creating a room: ${e}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    // if (!hasAccess) {
+    //   throw new Error('You do not have access to this document');
+    // }
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error in providing access to room ${error}`);
+  }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+    revalidatePath(`/documents/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error occured while updating room ${error}`);
+  }
+};
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    // if (!hasAccess) {
+    //   throw new Error('You do not have access to this document');
+    // }
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error in displaying rooms ${error}`);
+  }
+};
